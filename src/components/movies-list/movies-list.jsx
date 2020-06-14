c48617b8b6d0ee40a2a61a729a85a72b2014e4da
Andrey Vershinin <adw006@ya.ru>
2020-06-14 15:21:35 +0300

    create MovieCard component

--- src/components/movies-list/movies-list.jsx
+++ src/components/movies-list/movies-list.jsx
@@ -1,25 +1,12 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import MovieCard from '../movie-card/movie-card.jsx';
 
 const MoviesList = ({ movies }) => {
   return (
     <div className="catalog__movies-list">
       {movies.map((title) => (
-        <article key={title} className="small-movie-card catalog__movies-card">
-          <div className="small-movie-card__image">
-            <img
-              src="img/fantastic-beasts-the-crimes-of-grindelwald.jpg"
-              alt="Fantastic Beasts: The Crimes of Grindelwald"
-              width="280"
-              height="175"
-            />
-          </div>
-          <h3 className="small-movie-card__title">
-            <a className="small-movie-card__link" href="movie-page.html">
-              {title}
-            </a>
-          </h3>
-        </article>
+        <MovieCard key={title} title={title} />
       ))}
     </div>
   );
