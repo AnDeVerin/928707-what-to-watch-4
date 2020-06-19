e8511d6d41ed7ea64f212346cf157ace95951f1d
Andrey Vershinin <adw006@ya.ru>
2020-06-19 19:49:38 +0300

    add mock data and refactor movie-card component

---
+++ src/mocks/films.js
@@ -0,0 +1,34 @@
+export default [
+  {
+    title: 'Fantastic Beasts: The Crimes of Grindelwald',
+    thumbUrl: 'img/fantastic-beasts-the-crimes-of-grindelwald.jpg',
+  },
+  {
+    title: 'Bohemian Rhapsody',
+    thumbUrl: 'img/bohemian-rhapsody.jpg',
+  },
+  {
+    title: 'Macbeth',
+    thumbUrl: 'img/macbeth.jpg',
+  },
+  {
+    title: 'Aviator',
+    thumbUrl: 'img/aviator.jpg',
+  },
+  {
+    title: 'We need to talk about Kevin',
+    thumbUrl: 'img/we-need-to-talk-about-kevin.jpg',
+  },
+  {
+    title: 'What We Do in the Shadows',
+    thumbUrl: 'img/what-we-do-in-the-shadows.jpg',
+  },
+  {
+    title: 'Revenant',
+    thumbUrl: 'img/revenant.jpg',
+  },
+  {
+    title: 'Johnny English',
+    thumbUrl: 'img/johnny-english.jpg',
+  },
+];
