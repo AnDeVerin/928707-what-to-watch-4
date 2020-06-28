da2c6e80ce6906f4d2b0ce733a9dae4ce5ad89c6
Andrey Vershinin <adw006@ya.ru>
2020-06-28 11:13:00 +0300

    add tabs-content component and tests

---
+++ src/components/tabs-content/tabs-content.jsx
@@ -0,0 +1,141 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import getRating from '../../utils/rating-description.js';
+import ReviewCard from '../review-card/review-card.jsx';
+
+const TabsContent = ({ activeTab, movie }) => {
+  const { overview, reviews } = movie;
+
+  const ratingLevel = getRating(overview.rating.value);
+  const starring3 = overview.stars.slice(0, 3).join(', ');
+
+  switch (activeTab) {
+    case `overview`:
+      return (
+        <>
+          <div className="movie-rating">
+            <div className="movie-rating__score">{overview.rating.value}</div>
+            <p className="movie-rating__meta">
+              <span className="movie-rating__level">{ratingLevel}</span>
+              <span className="movie-rating__count">
+                {overview.rating.count} ratings
+              </span>
+            </p>
+          </div>
+
+          <div className="movie-card__text">
+            {overview.description.map((text, i) => (
+              <p key={text[0] + i}>{text}</p>
+            ))}
+
+            <p className="movie-card__director">
+              <strong>Director: {overview.director}</strong>
+            </p>
+
+            <p className="movie-card__starring">
+              <strong>Starring: {starring3} and other</strong>
+            </p>
+          </div>
+        </>
+      );
+
+    case `details`:
+      return (
+        <div className="movie-card__text movie-card__row">
+          <div className="movie-card__text-col">
+            <p className="movie-card__details-item">
+              <strong className="movie-card__details-name">Director</strong>
+              <span className="movie-card__details-value">
+                {overview.director}
+              </span>
+            </p>
+            <p className="movie-card__details-item">
+              <strong className="movie-card__details-name">Starring</strong>
+              <span className="movie-card__details-value">
+                {overview.stars.map((star, i) => (
+                  <span key={`${star}-${i}`}>
+                    {star} <br />
+                  </span>
+                ))}
+              </span>
+            </p>
+          </div>
+
+          <div className="movie-card__text-col">
+            <p className="movie-card__details-item">
+              <strong className="movie-card__details-name">Run Time</strong>
+              <span className="movie-card__details-value">
+                {overview.runTime}
+              </span>
+            </p>
+            <p className="movie-card__details-item">
+              <strong className="movie-card__details-name">Genre</strong>
+              <span className="movie-card__details-value">{movie.genre}</span>
+            </p>
+            <p className="movie-card__details-item">
+              <strong className="movie-card__details-name">Released</strong>
+              <span className="movie-card__details-value">
+                {movie.realeseYear}
+              </span>
+            </p>
+          </div>
+        </div>
+      );
+
+    case `reviews`:
+      const { length } = reviews;
+
+      return (
+        <div className="movie-card__reviews movie-card__row">
+          <div className="movie-card__reviews-col">
+            {length === 1 &&
+              reviews.map((review, i) => (
+                <ReviewCard key={review.text + i} review={review} />
+              ))}
+
+            {length > 1 &&
+              reviews
+                .slice(0, Math.ceil(length / 2))
+                .map((review, i) => (
+                  <ReviewCard key={review.text + i} review={review} />
+                ))}
+          </div>
+
+          {length > 1 && (
+            <div className="movie-card__reviews-col">
+              {reviews.slice(Math.ceil(length / 2)).map((review, i) => (
+                <ReviewCard key={review.text + i} review={review} />
+              ))}
+            </div>
+          )}
+        </div>
+      );
+  }
+
+  return null;
+};
+
+TabsContent.propTypes = {
+  activeTab: PropTypes.string.isRequired,
+  movie: PropTypes.shape({
+    title: PropTypes.string.isRequired,
+    thumbUrl: PropTypes.string.isRequired,
+    genre: PropTypes.string.isRequired,
+    realeseYear: PropTypes.number.isRequired,
+    posterUrl: PropTypes.string.isRequired,
+    coverUrl: PropTypes.string.isRequired,
+    overview: PropTypes.shape({
+      rating: PropTypes.shape({
+        value: PropTypes.number.isRequired,
+        count: PropTypes.number.isRequired,
+      }).isRequired,
+      description: PropTypes.arrayOf(PropTypes.string).isRequired,
+      director: PropTypes.string.isRequired,
+      stars: PropTypes.arrayOf(PropTypes.string.isRequired).isRequired,
+      runTime: PropTypes.string.isRequired,
+    }).isRequired,
+    reviews: PropTypes.array.isRequired,
+  }).isRequired,
+};
+
+export default TabsContent;
---
+++ src/components/tabs-content/tabs-content.test.js
@@ -0,0 +1,109 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import TabsContent from './tabs-content.jsx';
+
+const movie = {
+  title: `Fantastic Beasts: The Crimes of Grindelwald`,
+  thumbUrl: `img/fantastic-beasts-the-crimes-of-grindelwald.jpg`,
+  genre: `Adventure`,
+  realeseYear: 2018,
+  posterUrl: `https://m.media-amazon.com/images/M/MV5BYWVlMDI5N2UtZTIyMC00NjZkLWI5Y2QtODM5NGE5MzA0NmVjXkEyXkFqcGdeQXVyNzU3NjUxMzE@._V1_.jpg`,
+  coverUrl: `https://m.media-amazon.com/images/M/MV5BYTRiOTM4YzEtN2MwMC00MjMxLWIxM2ItMGE1YzA1MjNkN2YzXkEyXkFqcGdeQXVyNjQ4ODE4MzQ@._V1_SX1777_CR0,0,1777,999_AL_.jpg`,
+  trailer: `https://download.blender.org/durian/trailer/sintel_trailer-480p.mp4`,
+  overview: {
+    rating: {
+      value: 6.6,
+      count: 240,
+    },
+    description: [
+      `At the end of the first film, the powerful Dark wizard Gellert Grindelwald (Depp) was captured by MACUSA (Magical Congress of the United States of America), with the help of Newt Scamander (Redmayne). But, making good on his threat, Grindelwald escaped custody and has set about gathering followers, most unsuspecting of his true agenda: to raise pure-blood wizards up to rule over all non-magical beings. In an effort to thwart Grindelwald's plans, Albus Dumbledore (Law) enlists his former student Newt Scamander, who agrees to help, unaware of the dangers that lie ahead. Lines are drawn as love and loyalty are tested, even among the truest friends and family, in an increasingly divided wizarding world.`,
+    ],
+    director: `David Yates`,
+    stars: [
+      `Johnny Depp`,
+      `Eddie Redmayne`,
+      `Kevin Guthrie`,
+      `Carmen Ejogo`,
+      `Wolf Roth`,
+      `Eddie Redmayne`,
+      `Zoë Kravitz`,
+      `Callum Turner`,
+      `Derek Riddell`,
+      `Cornell John`,
+      `Ezra Miller`,
+    ],
+    runTime: `2h 14min`,
+  },
+  reviews: [
+    {
+      text: `Discerning travellers and Wes Anderson fans will luxuriate in the glorious Mittel-European kitsch of one of the director's funniest and most exquisitely designed movies in years.`,
+      author: `Kate Muir`,
+      date: `2016-12-24`,
+      rating: 8.9,
+    },
+    {
+      text: `Anderson's films are too precious for some, but for those of us willing to lose ourselves in them, they're a delight. "The Grand Budapest Hotel" is no different, except that he has added a hint of gravitas to the mix, improving the recipe.`,
+      author: `Bill Goodykoontz`,
+      date: `2015-11-18`,
+      rating: 8.0,
+    },
+    {
+      text: `It is certainly a magical and childlike way of storytelling, even if the content is a little more adult.`,
+      author: `Paula Fleri-Soler`,
+      date: `2016-12-20`,
+      rating: 7.6,
+    },
+    {
+      text: `Discerning travellers and Wes Anderson fans will luxuriate in the glorious Mittel-European kitsch of one of the director's funniest and most exquisitely designed movies in years.`,
+      author: `Kate Muir`,
+      date: `2018-02-02`,
+      rating: 9.2,
+    },
+    {
+      text: `Anderson's films are too precious for some, but for those of us willing to lose ourselves in them, they're a delight. "The Grand Budapest Hotel" is no different, except that he has added a hint of gravitas to the mix, improving the recipe.`,
+      author: `Bill Goodykoontz`,
+      date: `2019-12-18`,
+      rating: 8.4,
+    },
+    {
+      text: `It is certainly a magical and childlike way of storytelling, even if the content is a little more adult. Even if the content is a little more adult`,
+      author: `Paula Fleri-Soler`,
+      date: `2020-12-22`,
+      rating: 7.0,
+    },
+  ],
+};
+
+describe('TabsContent component', () => {
+  it('renders correctly on active tab Overview', () => {
+    const component = renderer
+      .create(<TabsContent movie={movie} activeTab={`overview`} />)
+      .toJSON();
+
+    expect(component).toMatchSnapshot();
+  });
+
+  it('renders correctly on active tab Details', () => {
+    const component = renderer
+      .create(<TabsContent movie={movie} activeTab={`details`} />)
+      .toJSON();
+
+    expect(component).toMatchSnapshot();
+  });
+
+  it('renders correctly on active tab Reviews', () => {
+    const component = renderer
+      .create(<TabsContent movie={movie} activeTab={`reviews`} />)
+      .toJSON();
+
+    expect(component).toMatchSnapshot();
+  });
+
+  it('renders nothing on unknown active tab', () => {
+    const component = renderer
+      .create(<TabsContent movie={movie} activeTab={`some unknown tab`} />)
+      .toJSON();
+
+    expect(component).toMatchSnapshot();
+  });
+});
