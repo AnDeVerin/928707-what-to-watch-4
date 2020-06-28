93612d74248d25972d66ea708d1cfd48640db56d
Andrey Vershinin <adw006@ya.ru>
2020-06-28 11:35:36 +0300

    rename rating-description module

--- src/components/tabs-content/tabs-content.jsx
+++ src/components/tabs-content/tabs-content.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import getRating from '../../utils/rating-description.js';
+import getRating from '../../utils/get-rating-description.js';
 import ReviewCard from '../review-card/review-card.jsx';
 
 const TabsContent = ({ activeTab, movie }) => {
