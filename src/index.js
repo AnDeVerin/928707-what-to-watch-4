d3b8439e8eb5d435251ab5e78ffbcf095d4ff317
Andrey Vershinin <adw006@ya.ru>
2020-07-07 20:51:46 +0300

    add redux and store to app

--- src/index.js
+++ src/index.js
@@ -1,10 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 
 import App from './components/app/app.jsx';
-import { PROMO_MOVIE, MOVIES } from './mocks/films.js';
+import { reducer } from './reducer.js';
+import { PROMO_MOVIE } from './mocks/films.js';
+
+const store = createStore(
+  reducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__
+    ? window.__REDUX_DEVTOOLS_EXTENSION__()
+    : (f) => f
+);
 
 ReactDOM.render(
-  <App promoMovie={PROMO_MOVIE} movies={MOVIES} />,
+  <Provider store={store}>
+    <App promoMovie={PROMO_MOVIE} />
+  </Provider>,
   document.querySelector(`#root`)
 );
