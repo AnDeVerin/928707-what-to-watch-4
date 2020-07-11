3ec14fba3b4af592d7f83255cafa7481feaa436e
Andrey Vershinin <adw006@ya.ru>
2020-07-11 15:12:47 +0300

    move delay constant to constants

--- src/constants.js
+++ src/constants.js
@@ -1,1 +1,2 @@
-export const MOVIE_LIST_COUNT = 8;
+export const MOVIE_LIST_COUNT = 8; // numbers of videos to show in movie list
+export const DELAY = 1000; // delay before playing video in video card
