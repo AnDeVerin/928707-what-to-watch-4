71b2fc55d020c08b66f8e4287e8f7ddceadbaf60
Andrey Vershinin <adw006@ya.ru>
2020-07-20 13:57:50 +0300

    add authStatus to app

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -6,18 +6,31 @@ import { connect } from 'react-redux';
 import Main from '../main/main.jsx';
 import MoviePage from '../movie-page/movie-page.jsx';
 import Modal from '../modal/modal.jsx';
+import SignIn from '../sign-in/sign-in.jsx';
 
 import { getSelectedMovie } from '../../reducer/app/selectors.js';
+import { getAuthStatus } from '../../reducer/user/selectors.js';
+
+import {
+  Operation as UserOperation,
+  AuthorizationStatus,
+} from '../../reducer/user/user.js';
+
+const isGuest = (status) => status === AuthorizationStatus.NO_AUTH;
 
 const App = (props) => {
-  const { selectedMovie } = props;
+  const { selectedMovie, authStatus, login } = props;
 
   const _renderApp = () => {
-    if (selectedMovie.title) {
-      return <MoviePage />;
-    }
+    if (isGuest(authStatus)) {
+      return <SignIn onSubmit={login} />;
+    } else {
+      if (selectedMovie.title) {
+        return <MoviePage />;
+      }
 
-    return <Main />;
+      return <Main />;
+    }
   };
 
   return (
@@ -36,6 +49,9 @@ const App = (props) => {
             onMovieSelect={onMovieSelect}
           /> */}
           </Route>
+          <Route exact path="/dev-signin">
+            <SignIn onSubmit={() => {}} />
+          </Route>
         </Switch>
       </BrowserRouter>
     </>
@@ -44,11 +60,20 @@ const App = (props) => {
 
 const mapStateToProps = (state) => ({
   selectedMovie: getSelectedMovie(state),
+  authStatus: getAuthStatus(state),
+});
+
+const mapDispatchToProps = (dispatch) => ({
+  login(authData) {
+    dispatch(UserOperation.login(authData));
+  },
 });
 
 App.propTypes = {
   selectedMovie: PropTypes.object.isRequired,
+  authStatus: PropTypes.string.isRequired,
+  login: PropTypes.func.isRequired,
 };
 
 export { App };
-export default connect(mapStateToProps)(App);
+export default connect(mapStateToProps, mapDispatchToProps)(App);
