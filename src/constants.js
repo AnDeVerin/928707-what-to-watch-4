019b1390d6ab1deccde4a3e688dcf8b13b9efe7d
Andrey Vershinin <adw006@ya.ru>
2020-07-26 22:03:36 +0300

    update api & constants

--- src/constants.js
+++ src/constants.js
@@ -1,2 +1,12 @@
 export const MOVIE_LIST_COUNT = 8; // numbers of videos to show in movie list
 export const DELAY = 1000; // delay before playing video in video card
+
+export const Error = {
+  UNAUTHORIZED: 401,
+};
+
+export const AppRoute = {
+  MAIN: '/',
+  LOGIN: '/login',
+  MYLIST: '/my-list',
+};
