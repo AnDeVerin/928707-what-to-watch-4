fabbbd1fd99aec65b7cd92f35d8e9f723f4c75af
Andrey Vershinin <adw006@ya.ru>
2020-07-26 22:06:54 +0300

    refactor header component

--- src/components/header/header.test.js
+++ src/components/header/header.test.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
+import { Router } from 'react-router-dom';
+import history from '../../history.js';
 import { Header } from './header.jsx';
 import { AuthorizationStatus } from '../../reducer/user/user.js';
 
 describe('Header component', () => {
   it('renders correctly for authenticated user', () => {
     const component = renderer
-      .create(<Header authStatus={AuthorizationStatus.AUTH} />)
+      .create(
+        <Router history={history}>
+          <Header authStatus={AuthorizationStatus.AUTH} />
+        </Router>
+      )
       .toJSON();
 
     expect(component).toMatchSnapshot();
@@ -14,7 +20,11 @@ describe('Header component', () => {
 
   it('renders correctly for unauthenticated user', () => {
     const component = renderer
-      .create(<Header authStatus={AuthorizationStatus.NO_AUTH} />)
+      .create(
+        <Router history={history}>
+          <Header authStatus={AuthorizationStatus.NO_AUTH} />
+        </Router>
+      )
       .toJSON();
 
     expect(component).toMatchSnapshot();
