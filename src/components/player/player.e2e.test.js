425564d323a26e3531f2561fe7885a191d71f14e
Andrey Vershinin <adw006@ya.ru>
2020-08-03 14:46:09 +0300

    fix warning in tests

--- src/components/player/player.e2e.test.js
+++ src/components/player/player.e2e.test.js
@@ -11,7 +11,6 @@ describe('Player component', () => {
     const player = mount(
       <Player
         isPlaying={true}
-        isMuted={true}
         poster={`https://m.media-amazon.com/images/M/MV5BMTA2NDc3Njg5NDVeQTJeQWpwZ15BbWU4MDc1NDcxNTUz._V1_SY1000_CR0,0,674,1000_AL_.jpg`}
         src={`https://upload.wikimedia.org/wikipedia/commons/transcoded/b/b3/Big_Buck_Bunny_Trailer_400p.ogv/Big_Buck_Bunny_Trailer_400p.ogv.360p.webm`}
       />
