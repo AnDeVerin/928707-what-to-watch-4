eeaf588ae8209a47d4fcabc7def4186acdef26c6
Andrey Vershinin <adw006@ya.ru>
2020-08-03 16:23:38 +0300

    update Header component

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -2,14 +2,14 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
-import { AppRoute } from '../../constants.js';
+import { AppRoute, HOST } from '../../constants.js';
 
-import { getAuthStatus } from '../../reducer/user/selectors.js';
+import { getAuthStatus, getUser } from '../../reducer/user/selectors.js';
 import { AuthorizationStatus } from '../../reducer/user/user.js';
 
 const isGuest = (status) => status === AuthorizationStatus.NO_AUTH;
 
-const Header = ({ authStatus, renderTitle = () => {} }) => (
+const Header = ({ authStatus, user, renderTitle = () => {} }) => (
   <header className="page-header movie-card__head">
     <div className="logo">
       <Link to={AppRoute.MAIN} className="logo__link">
@@ -30,8 +30,9 @@ const Header = ({ authStatus, renderTitle = () => {} }) => (
         <Link to={AppRoute.MYLIST}>
           <div className="user-block__avatar">
             <img
-              src="img/avatar.jpg"
+              src={`${HOST}${user.avatar_url}`}
               alt="User avatar"
+              title={user.email}
               width="63"
               height="63"
             />
@@ -44,10 +45,12 @@ const Header = ({ authStatus, renderTitle = () => {} }) => (
 
 const mapStateToProps = (state) => ({
   authStatus: getAuthStatus(state),
+  user: getUser(state),
 });
 
 Header.propTypes = {
   authStatus: PropTypes.string.isRequired,
+  user: PropTypes.object.isRequired,
   renderTitle: PropTypes.func,
 };
 
