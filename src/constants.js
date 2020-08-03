b8d280cfa1e090cefd8bc3516adac95a9c32f43e
Andrey Vershinin <adw006@ya.ru>
2020-08-03 16:21:57 +0300

    fix api

--- src/constants.js
+++ src/constants.js
@@ -1,8 +1,10 @@
 export const MOVIE_LIST_COUNT = 8; // numbers of videos to show in movie list
 export const DELAY = 1000; // delay before playing video in video card
+export const HOST = `https://4.react.pages.academy`;
 
 export const Error = {
   UNAUTHORIZED: 401,
+  BAD_REQUEST: 400,
 };
 
 export const AppRoute = {
